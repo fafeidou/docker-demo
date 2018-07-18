dbcf7c84c66ded3ae0011dfa73a04bafb7c17412
batman <943104990@qq.com>
2018-07-18 09:54:29 +0800

    update some bug

--- src/main/java/com/anxpp/demo/DockerApplication.java
+++ src/main/java/com/anxpp/demo/DockerApplication.java
@@ -1,13 +1,12 @@
 package com.anxpp.demo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,10 +16,10 @@ public class DockerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
 	}
-	@RequestMapping("/")
+	@GetMapping("/")
+	@ResponseBody
 	public String index(){
 		Integer integer = jdbcTemplate.queryForObject("select count(1) from city", Integer.class);
-
 		return integer.toString();
 	}
 }
