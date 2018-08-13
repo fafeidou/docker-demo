5a1588d9ed0af44bd82f8b4e517e126666b6f424
batman <943104990@qq.com>
2018-08-13 10:12:33 +0800

    docker hello world

--- src/main/java/com/anxpp/demo/DockerApplication.java
+++ src/main/java/com/anxpp/demo/DockerApplication.java
@@ -1,9 +1,7 @@
 package com.anxpp.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DockerApplication {
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	public static void main(String[] args) {
-		SpringApplication.run(DockerApplication.class, args);
-	}
-	@GetMapping("/")
-	@ResponseBody
-	public String index(){
-		Integer integer = jdbcTemplate.queryForObject("select count(1) from city", Integer.class);
-		return integer.toString();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "hello world!";
+    }
 }
